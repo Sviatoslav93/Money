af5a36df43fab3a5da637f128a57e30fee8c520a
Sviatoslav Kindrat <sviatoslav.kindrat@outlook.com>
2025-06-10 10:20:18 +0300

    fix naming

--- example/Example/Program.cs
+++ example/Example/Program.cs
@@ -1,29 +1,29 @@
 ﻿using Monetary;
 
-var money = new Money(100, Currency.Eur);
+var money1 = new Money(100, Currency.Eur);
 
 var money2 = new Money(100, Currency.Eur);
 
-var sum = money + money2;
+var sum = money1 + money2;
 Console.WriteLine($"Sum: {sum.Amount} {sum.Currency}");
 
-var difference = money - money2;
+var difference = money1 - money2;
 Console.WriteLine($"Difference: {difference.Amount} {difference.Currency}");
 
-var product = money * 2;
+var product = money1 * 2;
 Console.WriteLine($"Product: {product.Amount} {product.Currency}");
 
-var quotient = money / 2;
+var quotient = money1 / 2;
 Console.WriteLine($"Quotient: {quotient.Amount} {quotient.Currency}");
 
-var isEqual = money == money2;
+var isEqual = money1 == money2;
 Console.WriteLine($"Is Equal: {isEqual}");
 
-var isNotEqual = money != money2;
+var isNotEqual = money1 != money2;
 Console.WriteLine($"Is Not Equal: {isNotEqual}");
 
-var isGreater = money > money2;
+var isGreater = money1 > money2;
 Console.WriteLine($"Is Greater: {isGreater}");
 
-var isLess = money < money2;
+var isLess = money1 < money2;
 Console.WriteLine($"Is Less: {isLess}");    
